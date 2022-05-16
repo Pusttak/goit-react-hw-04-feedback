d0772339ff900130c02dbb9e30ec1b27ad4787e2
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-05-16 18:10:45 +0400

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  setRating = evt => {
+    const { name } = evt.target;
+
+    this.setState(prevState => {
+      return { [name]: prevState[name] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce(
+      (acc, feedback) => acc + feedback,
+      0
+    );
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.ceil((100 / this.countTotalFeedback()) * this.state.good) || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const buttonsName = Object.keys(this.state);
+
+    return (
+      <div
+        style={{
+          display: 'inline-flex',
+          flexDirection: 'column',
+          justifyContent: 'start',
+          marginTop: '150px',
+          alignItems: 'start',
+          color: '#010101',
+        }}
+      >
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={buttonsName}
+            onLeaveFeedback={this.setRating}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
